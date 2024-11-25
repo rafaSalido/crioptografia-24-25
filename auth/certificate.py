4c1d7f2ab1b896d3fd1766e18812274ef6b4f9c6
Pablo Cortés Rodríguez <cortesrodriguezpablo3@gmail.com>
2024-11-25 20:11:44 +0100

    create commit

--- auth/certificate.py
+++ auth/certificate.py
@@ -5,7 +5,7 @@
 
 def generate_certificate(user_name: str, user_public_key: str, entity_public_key: str) -> str:
     """
-    Generate a certificate for the user.
+    Generate a certificate for the user and return it.
     """
     
     pass
